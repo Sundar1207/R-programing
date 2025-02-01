045f9a5136115453e72d63500ea3f80c0c4ba098
S.Sundaramoorthy <157666440+Sundar1207@users.noreply.github.com>
2025-02-01 09:32:49 +0530

    Create unique.r

---
+++ ITA0407-Statistic  R progaraming/LOOPING, CONTROL STRUCTURES, FUNCTIONS, RECURSION    IN R /unique.r
@@ -0,0 +1,12 @@
+char_unique<-function(str)
+{
+   result<-unique(strsplit(str)[[]])
+}
+
+print(char_unique("programing"))
+
+
+# (str, NULL)[[1]] seems incorrect unless str is inside a list.
+# If str is a list, [[1]] extracts the first element.
+# If str is a character vector, [[1]] will throw an error.
+# NULL in indexing often means "nothing" and is ignored unless explicitly used in a list.

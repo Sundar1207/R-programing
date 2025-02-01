60c0b553625e41b16410c3265adfd9055979004a
S.Sundaramoorthy <157666440+Sundar1207@users.noreply.github.com>
2025-02-01 09:35:50 +0530

    Create  vector_matrix.r

---
+++ ITA0407-Statistic  R progaraming/LOOPING, CONTROL STRUCTURES, FUNCTIONS, RECURSION    IN R /vector_matrix.r
@@ -0,0 +1,12 @@
+# Create three vectors
+a <- c(1, 2, 3)
+b <- c(4, 5, 6)
+c <- c(7, 8, 9)
+
+# Combine them into a 3x3 matrix (column-wise)
+matrix_3x3 <- cbind(a, b, c)
+
+# Print the matrix
+print(matrix_3x3)
+
+#cbind(a, b, c) binds the vectors as columns to form a matrix.

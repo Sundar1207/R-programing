3383c53f4f53f651bb24f718544fe712bd951833
S.Sundaramoorthy <157666440+Sundar1207@users.noreply.github.com>
2025-02-27 10:16:45 +0530

    Create list.r

---
+++ Don't click/list.r
@@ -0,0 +1,11 @@
+# Create a list with different data types
+l <- list(10, "Hello", TRUE, 5.5)
+
+# Print the list
+print(l)
+
+# Access the second element
+print(l[[2]])
+
+# Check the type of the list
+print(class(l))

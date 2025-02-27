47c9f01153b91c608dc0acaf99b2d62913a189f2
S.Sundaramoorthy <157666440+Sundar1207@users.noreply.github.com>
2025-02-27 11:00:26 +0530

    Create factor.r

---
+++ Don't click/factor.r
@@ -0,0 +1,11 @@
+
+colors <- factor(c("Red", "Blue", "Red", "Green", "Blue"))
+
+# Print the factor
+print(colors)
+
+# Show the levels (categories)
+print(levels(colors))
+
+# Check the type
+print(class(colors))
